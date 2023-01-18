b92f30f84e1833d8b7e7381c39a9d0a310d707e5
sitedesign747 <63049450+sitedesign747@users.noreply.github.com>
2023-01-18 13:45:34 +0600

    Create

---
+++ src/main.java
@@ -0,0 +1,27 @@
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+        List<Integer> result = new ArrayList<>();
+
+        int count = 0;
+        for (int x : intList) {
+            if (x >= 0) {
+                if (x % 2 == 0) {
+                    result.add(x);
+                    count++;
+                }
+                continue;
+            }
+        }
+        Collections.sort(result);
+        System.out.println(result);
+    }
+}
